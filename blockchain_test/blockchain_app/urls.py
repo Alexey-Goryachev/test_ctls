e102f22fea41d534c3a6c3cfd84cf8d89d428b5b
Alex_Goryachev <alexey.i.goryachev@gmail.com>
2025-03-06 23:38:33 +0200

    update templates

--- blockchain_test/blockchain_app/urls.py
+++ blockchain_test/blockchain_app/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import  include, path
 
 urlpatterns = [
-    path('api/', include('services.urls')),
-    path('', include('coins.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include('services.urls')),
+    path('', include('coins.urls')),    
 ]
--- blockchain_test/coins/urls.py
+++ blockchain_test/coins/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "coins"
 
 urlpatterns = [
-    path('', views.CoinListView, name='coin_list'),
-    path('<str:ticker>/', views.CoinDetailView, name='coin_detail'),
+    path('', views.CoinListView.as_view(), name='coin_list'),
+    path('<str:ticker>/', views.CoinDetailView.as_view(), name='coin_detail'),
 ]
